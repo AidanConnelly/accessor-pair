c7f68e34a886634218d527c5d284bb521f18643a
a <a>
2025-04-07 16:01:57 +0100

    Change the syntax of the `field_accessors` macro to allow for autocomplete and usage detection by IDEs. Also allow composition of accessor pairs
    (note for autocompletion, it's currently incorrectly detected as a read usage rather than a write usage by RustRover)

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,23 @@
-pub trait AccessorPair<Struct, Field> {
-    type Getter: Fn(&Struct) -> &Field;
-    type Setter: Fn(&mut Struct) -> &mut Field;
-
+/// Note that `Field: 'static'` is only used for `compose` atm, so may have to be removed
+pub trait AccessorPair<Struct, Field: 'static> {
     fn get<'a>(&self, on: &'a Struct) -> &'a Field ;
 
     fn set<'a>(&self, on: &'a mut Struct) -> &'a mut Field ;
 
+    fn compose<'a, Next: 'static, Other: AccessorPair<Field, Next> + Clone + 'a>(&'a self, other: Other) -> (impl Fn(&Struct) -> &Next, impl Fn(&mut Struct) -> &mut Next) {
+        let other_for_get = other.clone();
+        (
+            move |s: &Struct| other_for_get.get(self.get(s)),
+            move |s: &mut Struct| other.set(self.set(s)),
+        )
+    }
 }
 
-impl<Struct, Field, Getter, Setter> AccessorPair<Struct, Field> for (Getter, Setter)
+impl<Struct, Field: 'static, Getter, Setter> AccessorPair<Struct, Field> for (Getter, Setter)
 where
     Getter: for<'a> Fn(&'a Struct) -> &'a Field,
     Setter: for<'a> Fn(&'a mut Struct) -> &'a mut Field,
 {
-    type Getter = Getter;
-    type Setter = Setter;
 
     fn get<'a>(&self, on: &'a Struct) -> &'a Field {
         self.0(on)
@@ -36,6 +39,11 @@ macro_rules! field_accessors {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    struct PottedFern {
+        fern: Fern,
+        pot_name: String,
+    }
 
     struct Fern {
         species: String,
@@ -68,5 +76,22 @@ mod tests {
         test_setter(species_accessor, &mut fern);
 
         assert_ne!(fern.species, "Horsetails");
+    }
+
+    #[test]
+    fn test_compose() {
+        let mut potted_fern = PottedFern{
+            fern: Fern { species: "Horsetails".to_string(), healthy: false },
+            pot_name: "Medium terracotta pot".to_string(),
+        };
+
+        let fern_accessor: _ = field_accessors!(|potted_fern: PottedFern| potted_fern.fern);
+        let species_accessor: _ = field_accessors!(|fern: Fern| fern.species);
+        let composed = fern_accessor.compose(species_accessor);
+
+        assert_eq!(composed.get(&potted_fern), "Horsetails");
+        *composed.set(&mut potted_fern) = "Equisetum".to_string();
+        assert_ne!(composed.get(&potted_fern), "Horsetails");
+
     }
 }
